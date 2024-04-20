ed7705159550b71a6753f48af206b56aa947b012
yaotutu <957810668@qq.com>
2024-04-20 16:01:53 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		"postcss-px-to-viewport-8-plugin": {
+			unitToConvert: "px",
+			viewportWidth: 375,
+			unitPrecision: 5,
+			propList: ["*"],
+			viewportUnit: "vw",
+			fontViewportUnit: "vw",
+			selectorBlackList: [],
+			minPixelValue: 1,
+			mediaQuery: false,
+			replace: true,
+			exclude: [],
+			landscape: true,
+			landscapeUnit: "vw",
+			landscapeWidth: 667,
+		},
+	},
+};
